9a0d2f49db18914f2cd1a5a83724d47f969d333e
creation <creation2224@gmail.com>
2025-05-23 20:36:55 +0530

    added analytics :

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import './index.css'
 import Weather from './components/Weather'
+import { Analytics } from "@vercel/analytics/next"
 const App = () => {
   return (
     <div className='app'>
+      <Analytics/>
       <Weather/>
     </div>
   )
